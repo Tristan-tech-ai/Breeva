90cf3243c06ef843f2991a1ab6017c0a8b49305a
Tristan <craftify.world.ti@gmail.com>
2026-03-05 17:31:31 +0700

    fix: remove .vite cache, add to gitignore

--- .vite/deps/leaflet.js
+++
@@ -1,3 +0,0 @@
-import { t as require_leaflet_src } from "./leaflet-src-BBAsf63a.js";
-
-export default require_leaflet_src();
--- .vite/deps/react.js
+++
@@ -1,3 +0,0 @@
-import { t as require_react } from "./react-E_hBH7Dd.js";
-
-export default require_react();
